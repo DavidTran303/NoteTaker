7629852c066231be95a38eebbef5b6d0e9a887f0
David Tran <davtran303@gmail.com>
2021-02-22 11:44:18 -0700

    new routes in apiROutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,31 @@
 const router = require('express').Router();
+const fs = require('fs');
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
-    res.json(getNotes);
+    fs.readFile('./db/db.json', function(err, data) {
+    res.json(JSON.parse(data));
+    if (err) throw err;
+  });
 });
 
 // POST "/api/notes", with current note
 router.post('/notes', (req, res) => {
-    res.json(handleNoteView);
+   var newNote = req.body
+    console.log("newNote")
+
+    fs.readFile('./db/db.json', function(err, data) {
+    res.json(JSON.parse(data));
+    if (err) throw err;
+
+    
+
+
+
+
+
+  });
+  
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
