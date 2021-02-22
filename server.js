80011d22c0600757e42c84da21493bc2da130fd5
David Tran <davtran303@gmail.com>
2021-02-21 17:16:29 -0700

    newfiles

--- server.js
+++ server.js
@@ -1,87 +1,17 @@
-// Dependencies]
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const http = require('http');
-const { title } = require('process');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// Sets up the Express App
+// Initialize the app and create a port
 const app = express();
-const PORT = 303;
+const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+// Set up body parsing, static, and route middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-//Notes Data
-const notes = [
-  {
-    title: " ",
-    text: " ",
-    id: " ",
-  },
-  {
-    title: " ",
-    text: " ",
-    id: " ",
-  },
-  {
-    title: " ",
-    text: " ",
-    id: " ",
-  },
-];
-
-
-// function to take a filepath and respond with html
-const renderHTML = (filePath, res) => {
-  return fs.readFile(`${__dirname}${filePath}`, (err, data) => {
-    if (err) throw err;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
-};
-
-
-
-const handleRequest = (req, res) => {
-  // Capture the url the request is made to
-  const path = req.url;
-
-  // When we visit different urls, call the function with different arguments
-  switch (path) {
-    case '/server':
-      return renderHTML(`${path}.html`, res);
-
-    case '/notes':
-    return renderHTML('/../html/notes.html', res)
-
-    default:
-      return renderHTML('/../html/index.html', res);
-  }
-};
-
-// Create the server, assign it to a variable called "server"
-const server = http.createServer(handleRequest);
-
-
-
-// Routes
-
-// Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-// Displays all notes
-app.get('/api/notes', (req, res) => res.json(notes));
-
-
-
-
-
-
-// Starts our server
-server.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`);
-});
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
