9e40254720bece0f4d39099f8e21afc71a29e909
David Tran <davtran303@gmail.com>
2021-02-22 16:18:09 -0700

    changed directory

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,12 @@ const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const addNotes = []
+
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', function(req, res) {
   
-   readFileAsync('/db/db.json', function(err, data) {
+   readFileAsync('./db/db.json', function(err, data) {
     res.json(getNotes(data));
     console.log(data)
     if (err) throw err;
@@ -22,13 +22,13 @@ router.get('/notes', function(req, res) {
 router.post('/notes', (req, res) => {
    let addNotes = req.body
 
-    readFileAsync('/db/db.json', function(err, data)  {
+    readFileAsync('./db/db.json', function(err, data)  {
     res.json(getNotes)
     if (err) throw err;
     });
 
     // console.log("GET DATA" + data)
-    writeFileAsync('/db/db.json', function(err, data) {
+    writeFileAsync('./db/db.json', function(err, data) {
       if(err) throw  err;
       res.send()
       notes.push(addNotes);
