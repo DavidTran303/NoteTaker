bb18a80a80272255ca294a67b8bd89f6c37ea31d
David Tran <davtran303@gmail.com>
2021-02-21 21:10:32 -0700

    new save button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
-
+    res.json(getNotes);
 });
 
 // POST "/api/notes", with current note
 router.post('/notes', (req, res) => {
-
+    res.json(handleNoteView);
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete('/notes/:id', (req, res) => {
-
+    res.json(deleteNote);
 });
 
 module.exports = router;
