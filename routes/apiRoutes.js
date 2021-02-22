b9f799826192ef7279476e7c44aeff45bffcdbf5
David Tran <davtran303@gmail.com>
2021-02-22 14:41:14 -0700

    new file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-const fs = require('fs');
-
-
-
-
-
-
-var newNoteData = require("../public/assets/js/index");
-
-
+const fs = require('fs')
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
+    
     fs.readFile('./db/db.json', function(err, data) {
     res.json(JSON.parse(data));
     if (err) throw err;
@@ -19,17 +11,12 @@ router.get('/notes', (req, res) => {
 
 // POST "/api/notes", with current note
 router.post('/notes', (req, res) => {
-   var newNote = req.body
-    console.log("newNote")
+   
+    // console.log("GET DATA" + data);
 
     fs.readFile('./db/db.json', function(err, data) {
     res.json(JSON.parse(data));
     if (err) throw err;
-
-    
-
-
-
 
 
   });
