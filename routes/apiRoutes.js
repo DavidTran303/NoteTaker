b002e4631c3476e6b6a66e601b336e65b3830170
David Tran <davtran303@gmail.com>
2021-02-22 18:20:08 -0700

    newfile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,13 +24,21 @@ router.post('/notes', (req, res) => {
    let newNote = req.body;
   // add an ID to newNote
 
-  fs.readFile("./db/db.json", function (err, data) {
+  fs.readFile("./db/db.json", "utf-8", function (err, data) {
     if (err) throw err;
-    const dataArray =  parse(newNote)
-    console.log(dataArray);
-    dataArray.push(newNote);
 
-    fs.writeFile()
+// use json.parse  to convert the data string to an array
+    newNote = JSON.parse(data);
+//  add the newNote to the data array. You'll use the push method to do this.
+    newNotes.push(data);
+
+    console.log(data);
+//     dataArray.push(newNote);
+
+
+
+// write the updated data array to your db.json file. Remember that this function takes 2 parameters 1) the path to the file 2) what should be written to the file. So if I had an object called myObject and I wanted it written to myFile.json then I would do so like this:
+    fs.writeFile("./db/db.json", newNote)
         // respond with the new note
 
   });
